b22e68a51e56c1a585afec9523f85abddb20f07d
Kai Blume <kai.blume@avionic-design.de>
2023-11-17 10:46:23 +0100

    feat: Add some Boxes

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     backgroundColor: "midnightblue",
   },
   box: {
-    width: 100,
+    width: "100%",
     height: 150,
     backgroundColor: "maroon",
   },
