26dc04e954a5a14c902cc3df5c2605e839483dd9
Kai Blume <kai.blume@avionic-design.de>
2023-11-17 10:39:10 +0100

    feat: add some boxes

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// App als SUB-Klasse der importierten Component (App erbt von Component)
+// Komponenten werden nicht mehr als Klassen geschrieben
+// export default class App extends Component {
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.square} />
+        <View style={styles.box} />
+        <View style={styles.box2} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 100,
+  },
+  square: {
+    width: 150,
+    height: 150,
+    backgroundColor: "midnightblue",
+  },
+  box: {
+    width: 100,
+    height: 150,
+    backgroundColor: "maroon",
+  },
+  box2: {
+    width: 200,
+    height: 250,
+    backgroundColor: "lightslategrey",
   },
 });
